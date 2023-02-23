9132158d9aa68e10f7c956b40f6831c78f20728a
ira-udaybanerjee <83913262+ira-udaybanerjee@users.noreply.github.com>
2023-02-23 15:57:54 +0530

    Add files via upload

--- SQLSERVER/Trigger_Demo.sql
+++ SQLSERVER/Trigger_Demo.sql
@@ -3,16 +3,21 @@ select * from Product
 select * from OrderDetails 
 --Insert and Update Trigger on Same Table
 
+--When a new row is being inserted in the OrderDetails table
+--I want to apply a check that the sale price of the product
+--should not be beyond 80% or 120% of original price
+--Example: Price 100, the Sale Price cannot greater than 120 or less than 80
+
 --Trigger is Database Object
-create trigger trg_CheckPriceOnInsert
+create trigger trg_CheckPrice
 on orderdetails
-for insert
+for insert, update
 as 
 begin
    declare @SalePrice money
    select @SalePrice = saleprice from inserted --10000
 
-   declare @ProductId int
+   declare @ProductId char(5)
    select @ProductId = productid from inserted --1
 
    declare @StdPrice money
@@ -25,63 +30,24 @@ begin
    
    if abs(@difference) > 0.20
    begin
-      print 'The discount or surplus can not be more than 20 percent of standard price' 
-	  rollback
+      /*print 'The discount or surplus can not be more than 20 percent of standard price' 
+	  rollback*/
+	  ;throw 99999, 'Invalid Sale Price', 16
    end
 end
 
---magic table (inserted)
---OrderId        ProductId           QtySold        SalePrice
---  18              1                  1             10000
- set identity_insert dbo.orders on
-
-begin transaction t1
-  begin try
-  
+set xact_abort on
+begin  transaction
     insert into Orders (OrderId, OrderDateTime,CustomerId,SalespersonId)
-    values (15, '2022-07-23', 3, 2)
+    values ('00010', getdate(), 'C0001', 'SP003')
 
 	--Exception should be raised by trigger
     insert into OrderDetails
-    values (15, 3, 1, 32000)
+    values ('00010', 'P0001',1 , 18200)
 
-    commit transaction t1
-  end try
-  begin catch
-      print ('Invalid sale price')
-	  rollback transaction t1
-  end catch
-
-  select * from Orders 
-  select * from orderdetails
-
-  select * from Product 
-
-
-create trigger trg_CheckPriceOnUpdate
-on orderdetails
-for update
-as 
-begin
-   declare @SalePrice money
-   select @SalePrice = saleprice from inserted
-
-   declare @ProductId int
-   select @ProductId = productid from inserted 
-
-   declare @StdPrice money
-   select @StdPrice = StdPrice from Product where ProductId = @ProductId 
-
-   declare @difference money
-
-   set @difference = (@SalePrice - @StdPrice) / @StdPrice 
-
-   if update(SalePrice) and abs(@difference) > 0.20
-   begin
-      print 'The discount or surplus can not be more than 20% of standard price'
-      rollback transaction
-   end
-end
+	insert into OrderDetails
+    values ('00010', 'P0002',2 , 18000)
+commit transaction
 
 --delete trigger
 create trigger trg_PreventCustomerDelete
@@ -107,103 +73,3 @@ end
 
 drop trigger trg_PreventUpdateBooks 
 
-update books
-set bookname = 'Indian Idol'
-where BookId = 'B1100'
-
-
-
-
-
-drop trigger trg_PreventCustomerDelete
-
-
-
-delete customer
-where CustomerId = 3
-
-
-select * from customer 
-
-select * from orders 
-
-insert into Customer
-values (5, 'Aditi', 'Park Road', 'Dehradun', '9634963490')
-
-
-delete customer 
-where CustomerId= 4
-
-select * from Customer 
-
-select * from orders 
-
-select * from orderdetails 
-
-  insert into orders 
-  values (getdate(), 1, 3)
-
-   declare @NewOrderId int
-   select @NewOrderId = SCOPE_IDENTITY()
-
-  insert into OrderDetails 
-  values (@NewOrderId, 3, 1, 80000)
-		  
-
-		  select * from orders
-
-		  select * from orderdetails 
-
-
-
-select * from books 
-
-alter table books
-add constraint chk_MinPrice check (Price >= 100)
-
-update books
-set price = 99
-where bookid = 'B1100'
-
-alter table books with nocheck
-add constraint chk_NoBiography 
-check (category <> 'Biography') 
-
-
-
-alter table books with nocheck
-add constraint chk_oldpublishdate 
-check (year(publish_date) > 1990) 
-
-
-
-
-
-
-
-update books
-set category = 'Biography'
-where bookid = 'B1100'
-
-
-
-select * from books 
-select * from orders 
-
-alter table orders with nocheck
-add constraint chk_NoSunday
-check (datename(dw,OrderDateTime) <> 'Sunday')
-
-
-update orders
-set OrderDateTime = '2022-07-24'
-where orderdatetime = 1
-
-
-
-
-insert into orders
-values (16, '2022-07-17', 2, 1)
-
-
-select * from orders 
