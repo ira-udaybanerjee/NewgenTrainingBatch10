65161f98a8c9606f5ceefd0fd6e7b21abef39d13
ira-udaybanerjee <83913262+ira-udaybanerjee@users.noreply.github.com>
2023-02-28 11:50:08 +0530

    Add files via upload

---
+++ Java/AccessSpec/Engine.java
@@ -0,0 +1,15 @@
+package package5;
+
+import package4.*;
+
+public class Engine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        ClassA aobj = new ClassA();
+        aobj.dosomething();
+        
+        package4.Engine eng = new package4.Engine();
+	}
+
+}
